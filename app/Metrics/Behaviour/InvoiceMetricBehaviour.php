f9e3cb27b775d1ca980d813431d28ccacd9364c8
Valeria <vale_0722@live.com>
2020-10-10 07:15:36 -0500

    refactor: delete .idea

--- app/Metrics/Behaviour/InvoiceMetricBehaviour.php
+++ app/Metrics/Behaviour/InvoiceMetricBehaviour.php
@@ -10,6 +10,19 @@ class InvoiceMetricBehaviour implements MetricContract
 {
     public function read(array $filters): Collection
     {
-        return Metric::readThreeLevelsMetric(Metric::getMetrics($filters));
+        $primary = $filters['primary'] ?? null;
+
+        $metrics =  Metric::filterByPrimaryId($primary)
+            ->whereBetween('date', [
+                $filters['from'] ?? now()->subMonth()->format('Y-m-d'),
+                $filters['to'] ?? now()->format('Y-m-d'),
+            ])
+            ->selectRaw('SUM(total) as `total`,
+                `date` as `date`,
+                `status`')
+            ->groupBy('status', 'date')
+            ->get(['date', 'status', 'total']);
+
+        return Metric::readThreeLevelsMetric($metrics);
     }
 }
